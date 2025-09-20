80019f7059f056bd95d04ccc5f5b514d64126ab3
sourena <sourena83negahban@gmail.com>
2025-09-21 01:39:05 +0330

    add show and hide header when scrolling

--- script.js
+++ script.js
@@ -4,3 +4,32 @@ const openMenu = document.querySelector(".openMenu");
 const removeMenu = document.querySelector(".removeMenu");
 menu.addEventListener("click", () => openMenu.classList.add("active"));
 removeMenu.addEventListener("click", () => openMenu.classList.remove("active"));
+
+
+// hide and show menu when scrolling
+const header = document.querySelector(".header");
+let lastScroll = 0;
+
+window.addEventListener("scroll", scrolling);
+
+function scrolling() {
+  const currentScroll = window.scrollY;
+
+  // when fixed to top of website
+  if (currentScroll <= 0) {
+    header.classList.remove("notActive");
+    header.classList.remove("active");
+  }
+  // when scroll to bottom
+  else if (currentScroll > lastScroll) {
+    header.classList.add("notActive");
+    header.classList.remove("active");
+  }
+  // scroll to top
+  else {
+    header.classList.remove("notActive");
+    header.classList.add("active");
+  }
+
+  lastScroll = currentScroll;
+}
